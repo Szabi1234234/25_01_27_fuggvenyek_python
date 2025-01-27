3d422cb5e7ec1d3bdb8332a5a4ad040d31bbd531
Szabi1234234 <toth.szabolcs1@diak.szbi-pg.hu>
2025-01-27 09:51:26 +0100

    add files

---
+++ 05_sort_vs_sorted.py
@@ -0,0 +1,13 @@
+L = [1, 5, 4, 2, 3]
+sorted_list = sorted(L)
+
+# print(f"Original list: {L}" )
+# print(sorted_list)
+
+print(L)
+L.sort()
+print(f"After sort: {L}" )
+print(L)
+
+#2. Feladat
+#Írj eljárást, amely paraméterül kapott számról eldönti, és a képernyőre kiírja, hogy negatív, pozitív vagy nulla-e!
---
+++ 06_eljaras.py
@@ -0,0 +1,22 @@
+#2. Feladat
+#Írj eljárást, amely paraméterül kapott számról eldönti, és a képernyőre kiírja, hogy negatív, pozitív vagy nulla-e!
+
+def eljaras(szam):
+    while True:
+        if szam
+            szam == ""
+        else:
+            szam = input('Adj meg egy számot: ')
+    if szam > 0:
+        print('Pozitív')
+    elif szam < 0:
+        print('Negatív')
+    else:
+        print('Nullával egyenlö')
+
+input_szam = int(input('Adj meg egy számot: '))
+
+eljaras(input_szam)
+while True:
+    input_szam = (" ")
+    break
